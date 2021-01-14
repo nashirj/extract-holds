f6ac94b6d309a0e559c63dc7cc4898ce2f92a39e
Nashir Janmohamed <nashirbass@gmail.com>
2021-01-14 09:27:40 -0500

    Add progress

--- NeuralClimb_scsukas8/holdDetector.py
+++ NeuralClimb_scsukas8/holdDetector.py
@@ -1,8 +1,8 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from util import openFile, showImage
+from util import openFile
 import colorsys
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -10,13 +10,13 @@ from mpl_toolkits.mplot3d import Axes3D
 def openImage(file_path=None):
     # Open Image
     file_path = openFile() if not file_path else file_path
-    img = cv2.imread(file_path,1)
+    img = cv.imread(file_path,1)
 
     # Image can be resized to a standard size to speed up processing.
     c = 1000.0/img.shape[0]
     x = int(img.shape[0] * c)
     y = int(img.shape[1] * c)
-    img = cv2.resize(img, (y,x))
+    img = cv.resize(img, (y,x))
 
     return img
 
@@ -24,7 +24,7 @@ def openImage(file_path=None):
 
 def buildDetector(minArea = 25):
     # Setup SimpleBlobDetector parameters.
-    params = cv2.SimpleBlobDetector_Params()
+    params = cv.SimpleBlobDetector_Params()
 
     # Change thresholds
     params.minThreshold = 0
@@ -48,11 +48,11 @@ def buildDetector(minArea = 25):
     params.minInertiaRatio = 0.05
 
     # Create a detector with the parameters
-    ver = (cv2.__version__).split('.')
+    ver = (cv.__version__).split('.')
     if int(ver[0]) < 3 :
-        detector = cv2.SimpleBlobDetector(params)
+        detector = cv.SimpleBlobDetector(params)
     else : 
-        detector = cv2.SimpleBlobDetector_create(params)
+        detector = cv.SimpleBlobDetector_create(params)
 
     return detector
 
@@ -61,33 +61,35 @@ def findHolds(img,detector = None):
     # Applying a gaussian blur removes some small impurities that
     # could fool the detection algorithm. It also smooths out the
     # color of each hold to make it more uniform.
-    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv.GaussianBlur(img, (5, 5), 0)
 
     # Using Otsu's method, the optimal threshold for the image can be found.
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    otsu, _ = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    otsu, _ = cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 
     # Applys edge detection to find the borders between the hold and the wall
     # Otsu's threshold is intended to be used as the higher threshold with a
     # lower:upper ratio of 1:2. L2gradient is included for more precise results.
-    edges = cv2.Canny(img,otsu, otsu * 2, L2gradient = True)
+    edges = cv.Canny(img,otsu, otsu * 2, L2gradient = True)
     # print(otsu)
 
     # Finds the contours of the image, without retaining the hierarchy or img
-    _, contours, _ = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv.findContours(edges,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
 
     # print("contours")
     # print(len(contours))
     # Applies convex hulls to each contour, ensuring each contour
     # is a closed polygon.
-    hulls = list(map(cv2.convexHull,contours))
+    hulls = list(map(cv.convexHull,contours))
     
     # Draws contours onto a blank canvas
     mask = np.zeros(img.shape,np.uint8)
 
-    cv2.drawContours(mask,hulls,-1,(255,255,255),-1)
+    cv.drawContours(mask,hulls,-1,(255,255,255),-1)
 
-    showImage(mask)
+    plt.imshow(mask)
+    plt.title("mask")
+    plt.show()
 
     if detector == None:
         # Set up the detector with default parameters.
@@ -98,63 +100,63 @@ def findHolds(img,detector = None):
 
 
 
-""" Color manipulations """
+# """ Color manipulations """
 
-def getColorBin(img, tl, br):
-    # Creates mask over image focus
-    mask = np.zeros(img.shape[:2], np.uint8)
-    mask[tl[1]:br[1], tl[0]:br[0]] = 255
+# def getColorBin(img, tl, br):
+#     # Creates mask over image focus
+#     mask = np.zeros(img.shape[:2], np.uint8)
+#     mask[tl[1]:br[1], tl[0]:br[0]] = 255
 
-    # Effecively quantizes the image when the histogram is made.
-    # Useful for grouping similar colors.
-    binLen = 4
-    numBins = 256 / binLen
+#     # Effecively quantizes the image when the histogram is made.
+#     # Useful for grouping similar colors.
+#     binLen = 4
+#     numBins = 256 / binLen
 
-    #Finds the most common color/in the histogram/for each color channel.
-    binColor = map(
-        lambda x: np.argmax(
-        [cv2.calcHist([img],[x],mask,[numBins],[0,256])])
-        ,[0,1,2])
+#     #Finds the most common color/in the histogram/for each color channel.
+#     binColor = map(
+#         lambda x: np.argmax(
+#         [cv.calcHist([img],[x],mask,[numBins],[0,256])])
+#         ,[0,1,2])
 
-    fullColor = map(lambda x: x * binLen, binColor)
+#     fullColor = map(lambda x: x * binLen, binColor)
 
-    return fullColor 
+#     return fullColor 
 
-def findColors(img,keypoints):
-    # If no keypoints return nothing
-    if (keypoints == []):
-        return []
+# def findColors(img,keypoints):
+#     # If no keypoints return nothing
+#     if (keypoints == []):
+#         return []
 
-    # Shift colorspace to HLS
-    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+#     # Shift colorspace to HLS
+#     hsv = cv.cvtColor(img, cv.COLOR_RGB2HLS)
 
-    # Preallocate space for color array corresponding to keypoints
-    colors = np.empty([len(keypoints),3])
+#     # Preallocate space for color array corresponding to keypoints
+#     colors = np.empty([len(keypoints),3])
 
-    # Iterates through the keypoints and finds the most common
-    # color at each keypoint.
-    for i, key in enumerate(keypoints):
-        x = int(key.pt[0])
-        y = int(key.pt[1])
+#     # Iterates through the keypoints and finds the most common
+#     # color at each keypoint.
+#     for i, key in enumerate(keypoints):
+#         x = int(key.pt[0])
+#         y = int(key.pt[1])
 
-        size = int(math.ceil(key.size)) 
+#         size = int(math.ceil(key.size)) 
 
-        #Finds a rectangular window in which the keypoint fits
-        br = (x + size, y + size)   
-        tl = (x - size, y - size)
+#         #Finds a rectangular window in which the keypoint fits
+#         br = (x + size, y + size)   
+#         tl = (x - size, y - size)
 
-        color = getColorBin(hsv,tl,br)
-        # for col in color:
-        #     print(col)
-        colors[i] = color
+#         color = getColorBin(hsv,tl,br)
+#         # for col in color:
+#         #     print(col)
+#         colors[i] = color
     
-    return colors
+#     return colors
 
 
 """ Visualization """
-def draw(img, keypoints):
+fewerdef draw(img, keypoints):
     # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the 
+    # cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the 
     # size of the circle corresponds to the size of blob
     for i, key in enumerate(keypoints):
         x = int(key.pt[0])
@@ -165,7 +167,7 @@ def draw(img, keypoints):
         #Finds a rectangular window in which the keypoint fits
         br = (x + size, y + size)   
         tl = (x - size, y - size)
-        cv2.rectangle(img,tl,br,(0,0,255),2)
+        cv.rectangle(img,tl,br,(0,0,255),2)
 
     #OpenCV uses BGR format, so that'll need to be reversed for display
     img = img[...,::-1]
@@ -178,38 +180,38 @@ def draw(img, keypoints):
 
 
 
-def plotColors(colors):
+# def plotColors(colors):
 
-    # Build 3D scatterplot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+#     # Build 3D scatterplot
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
     
-    # Initialize arrays
-    hs = []
-    ls = []
-    ss = []
+#     # Initialize arrays
+#     hs = []
+#     ls = []
+#     ss = []
 
-    # Color data is mapped between 0 and 1
-    colors = colors/256
+#     # Color data is mapped between 0 and 1
+#     colors = colors/256
 
-    for color in colors:
-        hs.append(color[0])
-        ls.append(color[1])
-        ss.append(color[2])
+#     for color in colors:
+#         hs.append(color[0])
+#         ls.append(color[1])
+#         ss.append(color[2])
 
-    # Regain RGB Values to color each data point
-    colorsRGB = map(colorsys.hls_to_rgb,hs,ls,ss) 
+#     # Regain RGB Values to color each data point
+#     colorsRGB = map(colorsys.hls_to_rgb,hs,ls,ss) 
 
-    # Plot points in HLS space
-    ax.scatter(hs, ls, ss, c=colorsRGB, marker='o')
+#     # Plot points in HLS space
+#     ax.scatter(hs, ls, ss, c=colorsRGB, marker='o')
 
-    ax.set_xlabel('Hue')
-    ax.set_ylabel('Lightness')
-    ax.set_zlabel('Saturation')
+#     ax.set_xlabel('Hue')
+#     ax.set_ylabel('Lightness')
+#     ax.set_zlabel('Saturation')
 
-    ax.set_xlim(0,1)
-    ax.set_ylim(0,1)
-    ax.set_zlim(0,1)
+#     ax.set_xlim(0,1)
+#     ax.set_ylim(0,1)
+#     ax.set_zlim(0,1)
 
-    plt.title("Color Space of Keypoints")
-    plt.show()
+#     plt.title("Color Space of Keypoints")
+#     plt.show()
--- NeuralClimb_scsukas8/util.py
+++ NeuralClimb_scsukas8/util.py
@@ -1,7 +1,7 @@
 #Utility file with convenient functions
 
 import matplotlib as plt
-import cv2
+import cv2 as cv
 from PIL import Image
 import tkinter as tk
 from tkinter import constants, filedialog
@@ -22,29 +22,8 @@ def openImage(file_path=None):
     if file_path == None:
         return None
 
-    image = cv2.imread(file_path,1)
+    image = cv2imread(file_path,1)
     return image
-
-def grab_from_webcam():
-    # Start webcam capture
-    cap = cv2.VideoCapture(0)
-
-    # Pause for webcam to adjust
-    time.sleep(1)
-
-    # Grabs a frame from the capture
-    retval, frame = cap.read()
-
-    # Release webcam
-    cap.release()
-
-    return (retval, frame)
-
-def grab_from_file(file_path = None):
-    frame = openImage(file_path)
-    retval = not frame is None
-
-    return (retval, frame)
 
 def resize(image, y = 0, x = 0):
     if y == 0:
@@ -52,12 +31,5 @@ def resize(image, y = 0, x = 0):
     if x == 0:
         r, c, _ = image.shape
         x = y * c / r;
-    image = cv2.resize(image, (y,x))
+    image = cv.resize(image, (y,x))
     return image
-
-def showImage(image, title = "Image"):
-    if image is None:
-        "Warning: Image is None"
-        return
-    cv2.imshow(title,image)
-    cv2.waitKey(0)
